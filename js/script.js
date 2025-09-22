12823e178c68db6595bd5c8faa455450af7801d6
Collins Kandie <collinskandie1@gmail.com>
2025-09-22 12:35:26 +0300

    multiple files and chnages

--- js/script.js
+++ js/script.js
@@ -1,62 +1,72 @@
-(function($) {
+(function ($) {
 
 	"use strict";
-	
-		// Mouse pointer
-		$(".wrapper-main").prepend("<div class='mouse-pointer'></div>");
 
-		function showCoords(event) {
-			var x = event.pageX;
-			var y = event.pageY;
-			var follower = $(".mouse-pointer");
-			follower.css({
-				left: x + (-12.5) + "px",
-				top: y + (-12.5) + "px",
-			});
+	// Mouse pointer
+	$(".wrapper-main").prepend("<div class='mouse-pointer'></div>");
 
+	function showCoords(event) {
+		var x = event.pageX;
+		var y = event.pageY;
+		var follower = $(".mouse-pointer");
+		follower.css({
+			left: x + (-12.5) + "px",
+			top: y + (-12.5) + "px",
+		});
+
+	}
+
+	$(window).on("mousemove", function (event) {
+		showCoords(event);
+	});
+
+	$("li, a, button, input, textarea, .navbar-toggles").mouseenter(function () {
+		$(".mouse-pointer").css("opacity", "0");
+		$("li, a, button, input, textarea, .navbar-toggles").mouseleave(function () {
+			$(".mouse-pointer").css("opacity", "1");
+		});
+	});
+
+
+	// fixed-menu
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > 50) {
+			$('.top-nav').addClass('fixed-menu');
+		} else {
+			$('.top-nav').removeClass('fixed-menu');
 		}
-		
-		$(window).on("mousemove", function(event) {
-			showCoords(event);
-		});
+	});
 
-		$("li, a, button, input, textarea, .navbar-toggles").mouseenter(function () {
-			$(".mouse-pointer").css("opacity", "0");
-			$("li, a, button, input, textarea, .navbar-toggles").mouseleave(function () {
-				$(".mouse-pointer").css("opacity", "1");
-			});
-		});
-		
-		
-		// fixed-menu
-		$(window).on('scroll', function () {
-			if ($(window).scrollTop() > 50) {
-				$('.top-nav').addClass('fixed-menu');
-			} else {
-				$('.top-nav').removeClass('fixed-menu');
-			}
-		});
 
-		
-		// blog-slider
-		$("#blog-slider").owlCarousel({
-			items:3,
-			itemsDesktop:[1199,3],
-			itemsDesktopSmall:[1000,2],
-			itemsMobile : [650,1],
-			navigationText:false,
-			autoPlay:true
+	// blog-slider
+	$("#blog-slider").owlCarousel({
+		items: 3,
+		itemsDesktop: [1199, 3],
+		itemsDesktopSmall: [1000, 2],
+		itemsMobile: [650, 1],
+		navigationText: false,
+		autoPlay: true
+	});
+
+	// customers-slider
+	$("#customers-slider").owlCarousel({
+		items: 5,
+		itemsDesktop: [1199, 5],
+		itemsDesktopSmall: [1000, 3],
+		itemsMobile: [650, 2],
+		navigationText: false,
+		autoPlay: true
+	});
+	// 🔽 Dropdown Hover Effect
+	document.querySelectorAll('.navbar .dropdown').forEach(function (dropdown) {
+		dropdown.addEventListener('mouseenter', function () {
+			this.classList.add('show');
+			this.querySelector('.dropdown-menu').classList.add('show');
 		});
-		
-		// customers-slider
-		$("#customers-slider").owlCarousel({
-			items:5,
-			itemsDesktop:[1199,5],
-			itemsDesktopSmall:[1000,3],
-			itemsMobile : [650,2],
-			navigationText:false,
-			autoPlay:true
+		dropdown.addEventListener('mouseleave', function () {
+			this.classList.remove('show');
+			this.querySelector('.dropdown-menu').classList.remove('show');
 		});
-		
-	
+	});
+
 })(window.jQuery);	
